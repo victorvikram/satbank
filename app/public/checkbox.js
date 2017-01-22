f3af39b27e76de03b766710d6f5d798b98b5d11b
vicviod <vicviod@gmail.com>
2017-01-22 14:30:47 -0500

    changes to index.ejs

---
+++ app/public/checkbox.js
@@ -0,0 +1,12 @@
+
+/*
+function preparePage() {
+  //$.getJSON("/")
+  document.getElementById();
+}
+
+
+window.onload = function() {
+  preparePage();
+}
+*/
---
+++ app/public/questions.js
@@ -0,0 +1,9 @@
+$(function() {
+  $getJSON('api', updateQuestions);
+  function updateFeedback(data) {
+    var output = '';
+    $.each(data, function(key, item) {
+
+    });
+  }
+});
