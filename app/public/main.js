028056c92fd40c7b47dd7a3401ae8786e6bf06f6
vicviod <vicviod@gmail.com>
2017-01-22 21:46:56 -0500

    updated main.js

--- app/public/main.js
+++ app/public/main.js
@@ -1,23 +1,47 @@
+function getCheckedBoxesOfClass(clss) {
+  var selected = [];
+  var checkButtons = document.getElementsByClassName(clss);
+  $.each(checkButtons, function(i, cb) {
+    if(cb.checked) {
+      selected.push(cb.id);
+    }
+  });
+  return selected;
+}
+
+function addHTMLElements(q, im) {
+  var img=document.createElement('img');
+  img.src = im;
+  img.id = q._id;
+  var li = document.createElement('li');
+  var checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.name = "name";
+  checkbox.class = "question"
+  checkbox.value = "value";
+  checkbox.id = q._id;
+  var tn = document.createTextNode(q._id);
+  li.appendChild(checkbox);
+  li.appendChild(tn);
+  li.appendChild(img);
+  imageLocation.appendChild(li);
+}
+
 $(function() {
   var refresh =  document.getElementById("refreshbutton");
-  var generate = document.getElementById("generateFromField")
+  var generate = document.getElementById("generateFromField");
   refresh.addEventListener("click", function(){
     // get all the checked boxes
-    var checkButtons = document.getElementsByClassName("input");
-    var selected = [];
-    $.each(checkButtons, function(i, cb) {
-      if(cb.checked) {
-        selected.push(cb.id);
-        console.log(cb.id + "\n");
-      }
-    });
+    var selected = getCheckedBoxesOfClass("tag");
     console.log(selected);
     refreshDisplay(selected);
   });
+  
   generate.addEventListener("click", function() {
     var idBox = document.getElementById("idBox");
-    var str = idBox.value
-    var ids = str.split(/,| /);
+    var str = idBox.value;
+    var selected = getCheckedBoxesOfClass("question");
+    var ids = selected.concat(str.split(/,| /));
     $.each(ids, function(index, id) {
       if(id === "") {
         ids.splice(index, 1);
@@ -28,10 +52,16 @@ $(function() {
 
 });
 
-function refreshDisplay() {
+function refreshDisplay(selected) {
+  var urlTail = ''
+  $.each(selected, function(i, t) {
+    urlTail += t;
+    urlTail += '&';
+  });
+  urlTail = urlTail.slice(0, -1);
   $.ajax({
     type: 'GET',
-    url: 'http://localhost:6969/api/questions?algebra',
+    url: 'http://localhost:6969/api/questions?' + urlTail,
     success: function(response) {
       console.log(response.data);
       var imageLocation = document.getElementById("questions");
@@ -46,21 +76,4 @@ function refreshDisplay() {
       });
     }
   });
-}
-
-function addHTMLElements(q, im) {
-  var img=document.createElement('img');
-  img.src = im;
-  img.id = q._id;
-  var li = document.createElement('li');
-  var checkbox = document.createElement('input');
-  checkbox.type = "checkbox";
-  checkbox.name = "name";
-  checkbox.value = "value";
-  checkbox.id = "id";
-  var tn = document.createTextNode(q._id);
-  li.appendChild(checkbox);
-  li.appendChild(tn);
-  li.appendChild(img);
-  imageLocation.appendChild(li);
 }
